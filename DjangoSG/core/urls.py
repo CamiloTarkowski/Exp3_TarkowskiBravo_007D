8fce0d7d3a97174fceb432d0e661bc538e1fc9d1
CamiloTarkowski <camilo.tarkowski@gmail.com>
2021-06-22 14:44:12 -0400

    implementando crud

--- DjangoSG/core/urls.py
+++ DjangoSG/core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import inicio, galeria
+from .views import inicio, galeria,crear_user,mod_user,ver_user,eliminar_user
 
 
 urlpatterns=[
     path('',inicio, name="inicio"),
     path('galeria/',galeria, name="galeria"),
+    path('crear_usuario/',crear_user,name="crear_user"),
+    path('mod_usuario/',mod_user,name="mod_user"),
+    path('ver_usuario/',ver_user,name="ver_user"),
+    path('eliminar_usuario/',eliminar_user,name="eliminar_user"),
     
 ]
--- DjangoSG/core/views.py
+++ DjangoSG/core/views.py
@@ -8,3 +8,15 @@ def inicio(request):
 
 def galeria(request):
     return render(request,'galeria.html')
+
+def crear_user(request):
+    return render(request,'core/crear_user.html')
+
+def mod_user(request):
+    return render(request,'core/mod_user.html')    
+
+def ver_user(request):
+    return render(request,'core/ver_user.html')
+
+def eliminar_user(request):
+    return render(request,'core/eliminar_user.html')
